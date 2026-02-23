79ce8585ada06ee1eb99e219491f5d950d16915e
fire8811 <jensonjones2002@gmail.com>
2026-02-22 19:07:30 -0700

    janky airtime logging demo

--- frontend/src/accelerometer-demo.tsx
+++ frontend/src/accelerometer-demo.tsx
@@ -1,10 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export function AccDemo() {
     const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
     const [gForce, setGForce] = useState(1);
-
     const [permission, SetPermission] = useState('unknown');
+
+    //getting airtime stuff
+    const [airtime, setAirtime] = useState(0); //airtime in MS
+    const [running, setRunning] = useState(false);
+    const startTimeRef = useRef<number | null>(null);
+
+    const inFreeFall = gForce <= 0.4;
 
     const requestPermission = async () => {
         const response = await (DeviceMotionEvent as any).requestPermission();
@@ -15,6 +21,19 @@ export function AccDemo() {
         }
     };
 
+    //airtime counting in milliseconds
+    useEffect(() => {
+      if (!running || !inFreeFall) return;
+
+      const start = Date.now();
+      const interval = setInterval(() => {
+        setAirtime(Date.now() - start);
+      }, 10);
+
+      return () => clearInterval(interval);
+    }, [running, inFreeFall]);
+
+    //g force detection stuff below
     const handleAcceleration = (event: DeviceMotionEvent) => {
       const acc = event.accelerationIncludingGravity;
       
@@ -32,15 +51,19 @@ export function AccDemo() {
       }
     };
 
-
     return (
     <div>
         <h2>G Force Demo</h2>
       {permission !== 'granted' && (
         <button onClick={requestPermission}>Enable Acceleromteter</button>
       )}
+
+      {gForce < 0.3 && <h1 style={{ color: "red", fontWeight: "bold" }}>FREE FALL</h1>} 
+
       <h1>G FORCE: {gForce.toFixed(2)}</h1>
-        
+      
+      <button onClick={() => {setAirtime(0); setRunning(true);}}>Enable Airtime Recording</button>
+      <h3>Airtime: {airtime} ms</h3>
     </div>
     );
 }
