14e7f3404a9027e632a77e16e776660527c270a2
fire8811 <jensonjones2002@gmail.com>
2026-02-26 19:39:54 -0700

    demo screen

---
+++ frontend/src/demo/demo.tsx
@@ -0,0 +1,13 @@
+import { GyroDemo } from './gyroscope-demo'
+import { AccDemo } from './accelerometer-demo'
+
+export function Demo() {
+    return (
+        <div className='Demo'>
+            <GyroDemo />
+            <AccDemo />
+        </div>
+ 
+    )
+    
+}
