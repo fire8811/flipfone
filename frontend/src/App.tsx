46fb0b8bf3dc5a00e2a8022fdf277715bcc8541e
fire8811 <jensonjones2002@gmail.com>
2026-03-12 20:01:11 -0600

    add warning popup component to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import { Demo } from "./demo/demo";
-import { Game } from "./Game/game";
+import { Game } from './Game/game';
+import { WarningModal } from './WarningModal'
+
 
 function App() {
 	// const [page, setPage] = useState<"home" | "demo" | "game">("home");
+  const [acknowledged, setAcknowledged] = useState(false)
 
 	return (
 		<div className="App">
+			{!acknowledged && <WarningModal onAcknowledge={() => setAcknowledged(true)} />}
 			{/* <nav style={{ display: "flex", gap: "1rem", padding: "1rem", borderBottom: "1px solid #ccc" }}>
 				<button onClick={() => setPage("demo")}>Demo</button>
 				<button onClick={() => setPage("game")}>Game</button>
